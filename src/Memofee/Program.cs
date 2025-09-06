e4c03f80207639abc5a9213c2f8ca0e8103454c0
Yuta Matsumura <tkmbmbr@gmail.com>
2025-09-07 08:08:13 +0900

    add aspire

--- src/Memofee/Program.cs
+++ src/Memofee/Program.cs
@@ -3,12 +3,16 @@ using Memofee.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.AddServiceDefaults();
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 
 var app = builder.Build();
+
+app.MapDefaultEndpoints();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
